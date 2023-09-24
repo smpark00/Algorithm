d28037f728e0a4c5bb58ba47f3ec508c0e385035
SeungMin Park <tonydragon@naver.com>
2023-09-25 02:26:06 +0900

    A/B 문제

---
+++ Bronze 5/BaekJoon1008/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        double a,b;
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextDouble();
+        b = sc.nextDouble();
+        System.out.printf(String.valueOf(a/b));
+
+    }
+}
