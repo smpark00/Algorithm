8fc54612ba46ad1016bbebd68f5af7d7f68f7576
SeungMin Park <tonydragon@naver.com>
2023-11-21 19:46:45 +0900

    9로 나눈 나머지

--- LV0/Rest9/src/Solution.java
+++ LV0/Rest9/src/Solution.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Solution {
+import java.util.Arrays;
+import java.util.Scanner;
+
+class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String number = sc.next();
+        System.out.println(solution(number));
+    }
+    public static int solution(String number) {
+        int answer = 0;
+        char[] num = new char[number.length()];
+        for(int i =0; i<number.length(); i++) {
+            num[i] = number.charAt(i);
+            answer += Integer.parseInt(String.valueOf(num[i]));
+        }
+        String a = String.valueOf(answer);
+        return answer%9;
+    }
 }
