eb38b8530881977b43063f91825472d8b936dc9c
SeungMin Park <tonydragon@naver.com>
2023-11-21 19:46:08 +0900

    문자열 뒤집기

---
+++ LV0/MakeArray2/src/Solution.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+class Solution {
+    public static void main(String[] args) {
+
+    }
+    public static int[] solution(int l, int r) {
+        int[] answer = {};
+        String rnum = String.valueOf(r);
+
+        return Integer.parseInt(rnum);
+    }
+}
