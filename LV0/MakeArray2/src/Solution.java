3fb7ba2a2d4d356abc8ff44dc1a768e5fb1de103
SeungMin Park <tonydragon@naver.com>
2023-11-22 18:15:06 +0900

    배열만들기

--- LV0/MakeArray2/src/Solution.java
+++ LV0/MakeArray2/src/Solution.java
@@ -1,12 +1,34 @@
 import java.util.Arrays;
+import java.util.Scanner;
+
 class Solution {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int l = sc.nextInt();
+        int r = sc.nextInt();
+        System.out.printf(Arrays.toString(solution(l,r)));
 
     }
     public static int[] solution(int l, int r) {
-        int[] answer = {};
-        String rnum = String.valueOf(r);
+        int[] answer = new int [10000];
 
-        return Integer.parseInt(rnum);
+        int count = 0;
+        for(int i=l; i<= r; i++){
+            int check = 0;
+            char [] num = String.valueOf(i).toCharArray();
+            for(int j=0; j< num.length; j++){
+                if(num[j] == '5' || num[j] == '0'){
+                    check++;
+                }
+                if(check == num.length){
+                    answer[count] += i;
+                    count++;
+                }
+            }
+        }
+        if(count == 0){
+            return new int[]{-1};
+        }
+        return Arrays.copyOf(answer, count);
     }
 }
