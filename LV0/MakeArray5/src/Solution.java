1e40639cb3e4e101e426e18ebe8d203d2ad1216a
SeungMin Park <tonydragon@naver.com>
2023-11-24 14:43:58 +0900

    배열 만들기 5

--- LV0/MakeArray5/src/Solution.java
+++ LV0/MakeArray5/src/Solution.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Solution {
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(String[] intStrs, int k, int s, int l) {
+        int[] answer = new int [10000];
+        int size = 0;
+
+        for (String intStr : intStrs) {
+            char[] arr = new char[intStr.length()];
+            for (int j = 0; j < intStr.length(); j++) {
+                arr[j] = intStr.charAt(j);
+            }
+            String str = "";
+            for (int t = s; t < l+s&& t < intStr.length(); t++) {
+                str += arr[t];
+            }
+            if (Integer.parseInt(str) > k) {
+                answer[size] = Integer.parseInt(str);
+                size++;
+            }
+        }
+
+        return Arrays.copyOf(answer, size);
+    }
 }
