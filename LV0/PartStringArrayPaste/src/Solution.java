bb8883b5996253cd804d0753ae7fd9c6a5bd3a03
SeungMin Park <tonydragon@naver.com>
2023-11-24 14:44:52 +0900

    부분 문자열 이어 붙여 문자열 만들기

--- LV0/PartStringArrayPaste/src/Solution.java
+++ LV0/PartStringArrayPaste/src/Solution.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Solution {
+class Solution {
+    public String solution(String[] my_strings, int[][] parts) {
+        String answer = "";
+        for(int i=0; i< my_strings.length; i++){
+            for(int j= parts[i][0]; j<=parts[i][1]; j++){
+                answer += my_strings[i].charAt(j);
+            }
+        }
+
+        return answer;
+    }
 }
