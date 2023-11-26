e62a20bd0df8504bb6d453a3f658c0a0a8d0c102
SeungMin Park <tonydragon@naver.com>
2023-11-26 14:19:57 +0900

    11일차

--- LV0/CountStringnum/src/Solution.java
+++ LV0/CountStringnum/src/Solution.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Solution {
+class Solution {
+    public int[] solution(String my_string) {
+        int[] answer = new int [52];
+        for(int i=0; i<my_string.length(); i++){
+            char str = my_string.charAt(i);
+            int strascii = (byte) str;
+            if(strascii >= 'a'){
+                answer[strascii - 'A' -6] += 1;
+            }else{
+                answer[strascii - 'A'] += 1;
+            }
+        }
+        return answer;
+    }
 }
