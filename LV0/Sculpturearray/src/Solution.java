117aa0a46c7508d4e5269efb424dbea091dcd84e
SeungMin Park <tonydragon@naver.com>
2023-11-29 18:27:58 +0900

    배열 조각하기

--- LV0/Sculpturearray/src/Solution.java
+++ LV0/Sculpturearray/src/Solution.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Solution {
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] arr, int[] query) {
+
+        for(int i=0; i< query.length; i++){
+            if(i %2 ==0){
+                arr = Arrays.copyOf(arr, query[i]+1);
+            } else {
+                arr = Arrays.copyOfRange(arr, query[i], arr.length);
+            }
+        }
+
+        return arr;
+    }
 }
