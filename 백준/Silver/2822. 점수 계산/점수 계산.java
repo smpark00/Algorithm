cc2a68c860f1743d6f879d75043c1f07c7e6b5b3
SeungMin Park <126854215+smpark00@users.noreply.github.com>
2024-01-19 01:17:47 +0900

    [Silver V] Title: 점수 계산, Time: 208 ms, Memory: 17704 KB -BaekjoonHub

--- 백준/Silver/2822. 점수 계산/점수 계산.java
+++ 백준/Silver/2822. 점수 계산/점수 계산.java
@@ -3,36 +3,35 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] score = new int[8];
+        int [] score = new int [8];
 
-        for (int i = 0; i < 8; i++) {
+        for(int i=0; i<8; i++){
             score[i] = sc.nextInt();
         }
 
-        int[] arrayscore = Arrays.copyOf(score, score.length);
+        int [] arrayscore = Arrays.copyOf(score, score.length);
         Arrays.sort(arrayscore);
         int total = 0;
-        int[] array = new int[5];
+        int [] array = new int [5];
 
-        for (int i = 3; i < 8; i++) {
+        for(int i=3; i<8; i++){
             total += arrayscore[i];
-        }
-
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 8; j++) {
-                if (arrayscore[i + 3] == score[j]) {
-                    array[i] = j + 1;
+            for(int j=0; j<8; j++){
+                if(arrayscore[i] == score[j]){
+                    array[i-3] = j+1;
                 }
             }
         }
 
         Arrays.sort(array);
-        System.out.println(total);
-        for (int i = 0; i < 5; i++) {
+        System.out.println(String.valueOf(total));
+
+        for(int i=0; i<5; i++){
             System.out.print(array[i]);
-            if (i != 4) {
+            if(i!=4){
                 System.out.print(" ");
             }
         }
+
     }
 }
