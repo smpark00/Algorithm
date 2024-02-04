5e2f5f6900caa81155ad490cf1583d9be00f047b
SeungMin Park <126854215+smpark00@users.noreply.github.com>
2024-02-04 13:06:14 +0900

    [level 2] Title: 올바른 괄호, Time: 20.84 ms, Memory: 54.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,23 @@
+import java.util.Stack;
+
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        Stack<String> stack = new Stack<>();
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i) == '('){
+                stack.push(String.valueOf(1));
+            } else if (s.charAt(i) == ')') {
+                if(stack.isEmpty()){
+                    answer = false;
+                    break;
+                }
+                stack.pop();
+            }
+        }
+        if(!stack.isEmpty()){
+            answer = false;
+        }
+        return answer;
+    }
+}
