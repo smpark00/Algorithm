68d2584a553cfe070a6e43b281a025cdb4b865da
SeungMin Park <126854215+smpark00@users.noreply.github.com>
2024-02-09 18:05:58 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 524 ms, Memory: 71240 KB -BaekjoonHub

---
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        sb.append((int)(Math.pow(2,n)-1)).append('\n');
+
+        Hanoi(n, 1, 2, 3);
+        System.out.print(sb);
+    }
+    private static void Hanoi(int n, int start, int mid, int to) {
+        if(n==1){
+            sb.append(start+" "+to+"\n");
+            return;
+        }
+
+        Hanoi(n-1, start, to, mid);
+
+        sb.append(start+" "+to+"\n");
+
+        Hanoi(n-1, mid, start, to);
+    }
+}
