f3d4bf2dc4f23fd77808767d62d7a0bab679f83a
SeungMin Park <126854215+smpark00@users.noreply.github.com>
2024-02-21 17:29:49 +0900

    [Silver III] Title: N과 M (1), Time: 280 ms, Memory: 22504 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N,M;
+    static boolean [] check;
+    static int[] num;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        check = new boolean[N];
+        num = new int[M];
+
+        backtracking(0);
+        System.out.print(sb);
+    }
+
+    private static void backtracking(int length) {
+        if(length == M){
+            for (int j : num) {
+                sb.append(j).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i=0; i<N; i++){
+            if(!check[i]) {
+                check[i] = true;
+                num[length] = i + 1;
+                backtracking(length + 1);
+                check[i] = false;
+            }
+        }
+    }
+}
