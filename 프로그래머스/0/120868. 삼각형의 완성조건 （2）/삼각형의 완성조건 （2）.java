17dc0b04c9436b745333da35f3cf6e381e247dcb
SeungMin Park <126854215+smpark00@users.noreply.github.com>
2024-02-29 21:16:08 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.44 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] sides) {
+        int answer = 0;
+        Arrays.sort(sides);
+        
+        for(int i=sides[1]-sides[0]+1; i<sides[0]+sides[1]; i++){
+            answer++;
+        }
+        
+        return answer;
+    }
+}
